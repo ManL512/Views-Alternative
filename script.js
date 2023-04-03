4f9c4cbc4d96e4c064b7de935f865df69495fe74
Miguel <miguelnavas933@gmail.com>
2023-04-03 11:25:37 -0500

    Boton funcional de notificaciones

--- script.js
+++ script.js
@@ -21,3 +21,31 @@ logoutBtn.addEventListener("click", function() {
 // Muestra la ventana emergente
 userPopup.style.display = "block";
 });
+
+const notificationsBtn = document.querySelector('.social_media ion-icon[name="notifications-outline"]');
+notificationsBtn.addEventListener('click', () => {
+const notificationsPopup = document.createElement('div');
+notificationsPopup.innerHTML = `
+    <h3>Notificaciones:</h3>
+    <ul>
+    <li>Tienes 3 nuevas notificaciones</li>
+    <li>Se ha creado un nuevo usuario</li>
+    <li>El sistema ha detectado un error</li>
+    </ul>
+    <button>Ver todas las notificaciones</button>
+`;
+notificationsPopup.style.position = 'absolute';
+notificationsPopup.style.top = '50px';
+notificationsPopup.style.right = '20px';
+notificationsPopup.style.background = '#fff';
+notificationsPopup.style.border = '1px solid #000';
+notificationsPopup.style.padding = '10px';
+notificationsPopup.style.display = 'none';
+notificationsPopup.style.maxHeight = '300px';
+notificationsPopup.style.overflowY = 'scroll';
+document.body.appendChild(notificationsPopup);
+notificationsPopup.style.display = 'block';
+});
+
+
+/* SCRIPT Cliente */
